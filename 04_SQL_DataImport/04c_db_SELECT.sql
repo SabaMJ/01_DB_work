6f0c3a3a08850af8cc66ac7e67d89447ba6e2163
SabaMJ <rrrsaba@gmail.com>
2024-08-02 10:29:42 +0200

    kommit ++

--- 04_SQL_DataImport/04c_db_SELECT.sql
+++ 04_SQL_DataImport/04c_db_SELECT.sql
@@ -7,8 +7,8 @@ SELECT * FROM boo.cats;
 /* UPDATE --> Änderung(en) in bestehender Tab. */
 
 
-UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
-UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
+-- UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
+-- UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 */
 /* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
 -- #SELECT * FROM boo.cats; -- gesamte Tabelle
@@ -17,19 +17,19 @@ UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 -- #SELECT cat_name AS "Katzen" FROM boo.cats; 
 
 /*Filtern durch WHERE*/
-SELECT 
-    cat_name AS "Katzen",
-    age AS "Alter"
-FROM boo.cats
-#WHERE id = 1 
+-- SELECT 
+--     cat_name AS "Katzen",
+--     age AS "Alter"
+-- FROM boo.cats
+-- #WHERE id = 1 
 -- #WHERE fur_color = "grey"
 -- #WHERE fur_color = "alpinaweiss"
 -- WHERE age >= 30
 ; 
 
 /* Delete - DS löschen | Vorsicht!! */
--- DELETE FROM boo.cats WHERE id = 2; -- Filter
+DELETE FROM boo.cats WHERE id = 2; -- Filter
 -- DELETE FROM boo.cats -- Alle Daten sind weg!!
 
--- SELECT * FROM boo.cats;
+SELECT * FROM boo.cats;
 
